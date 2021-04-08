d845566217e9e47a4909e61a71d8bafb04ea9962
Rachel Diaz <Rachel7113@gmail.com>
2021-04-08 17:02:48 -0400

    Created seed files & function to seed db

---
+++ seeds/index.js
@@ -0,0 +1,25 @@
+
+const sequelize = require("../config/connection");
+const { User, Posts } = require("../models");
+const userData = require("./user-seeds.json");
+const postData = require("./blogPosts-seeds.json");
+
+
+const seedDatabase = async () => {
+    try {
+        await sequelize.sync({ force: false });
+
+        await User.bulkCreate(userData, {
+            individualHooks: true,
+            returning: true,
+        });
+
+        await Posts.bulkCreate(postData),
+
+            process.exit(0);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+seedDatabase();
