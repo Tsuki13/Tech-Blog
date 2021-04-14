24f39cc8322bb3f8685f58154ec667a945b103ee
Rachel Diaz <Rachel7113@gmail.com>
2021-04-14 13:05:30 -0400

    added dashboard & create post routes

--- controllers/viewRoutes/index.js
+++ controllers/viewRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Posts } = require("../../models");
+const { authCheck } = require("../../utils/auth");
 
 
 // Get home page with blog posts
@@ -33,5 +34,37 @@ router.get("/signup", (req, res) => {
         loggedIn: req.session.loggedIn,
     });
 });
+
+router.get("/dashboard", authCheck, async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: { id: req.session.user_id },
+            include: { model: Posts },
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render("dashboard", {
+            user,
+            loggedIn: req.session.loggedIn,
+            user_id: req.session.user_id
+        })
+
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+router.get("/createPost", authCheck, async (req, res) => {
+    try {
+        res.render("createPost", {
+            loggedIn: req.session.loggedIn,
+            user_id: req.session.user_id
+        });
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
 
 module.exports = router
