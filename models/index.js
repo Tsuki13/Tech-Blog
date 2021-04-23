a62e6aa80944e3b6232b37d39729fb9ea87384a4
Rachel Diaz <Rachel7113@gmail.com>
2021-04-23 10:46:35 -0400

    Added comment model and relationships

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./Users");
 const Posts = require('./Posts');
+const Comment = require('./Comment');
 
 
 User.hasMany(Posts, {
@@ -10,4 +11,20 @@ Posts.belongsTo(User, {
     foreignKey: "user_id"
 });
 
-module.exports = { User, Posts };
+User.hasMany(Comment, {
+    foreignKey: "user_id"
+});
+
+Posts.hasMany(Comment, {
+    foreignKey: "post_id"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+Comment.belongsTo(Posts, {
+    foreignKey: "post_id"
+})
+
+module.exports = { User, Posts, Comment };
